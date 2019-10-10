9f147a777a9bbbe4a8de7cd0f6f09f8e3394030c
seeand <kdsfoxy@gmail.com>
2019-10-09 20:58:18 -0500

    dwr simple configuration

---
+++ src/main/java/com/mx/direct/web/remoting/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.mx.direct.web.remoting;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
